6565d870c731bf29295bad7eff3c1a0281b3748c
ngoc04092002 <vungoc23387@gmail.com>
2023-02-27 00:25:22 +0700

    create table client foreign key dailyPostEntity

---
+++ src/main/java/ttcs/btl/model/client/ClientEntity.java
@@ -0,0 +1,40 @@
+package ttcs.btl.model.client;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import ttcs.btl.model.dailyPost.DailyPostEntity;
+
+import java.util.Set;
+
+@Entity
+@Table(name="clients")
+@Data
+public class ClientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="username", nullable = false)
+    private String username;
+
+    @Column(name="email", nullable = false)
+    private String email;
+
+    @Column(name="address", nullable = false)
+    private String address;
+
+    @Column(name="gender", nullable = false)
+    private String gender;
+
+    @Column(name="sdt")
+    private String sdt;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="role")
+    private String role;
+
+    @OneToMany(mappedBy = "clientEntity", cascade = CascadeType.ALL)
+    private Set<DailyPostEntity> dailyPostEntitySet;
+}
--- src/main/java/ttcs/btl/model/dailyPost/DailyPostEntity.java
+++ src/main/java/ttcs/btl/model/dailyPost/DailyPostEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ttcs.btl.model.client.ClientEntity;
 
 import java.util.Date;
 import java.util.Set;
@@ -31,11 +32,15 @@ public class DailyPostEntity {
     @Column(name="poster_name", nullable = false)
     private String posterName;
 
-    @OneToMany(mappedBy = "dailyPostEntity")
+    @OneToMany(mappedBy = "dailyPostEntity", cascade = CascadeType.ALL)
     private Set<Likes> likes;
 
-    @OneToMany(mappedBy = "dailyPostEntity")
+    @OneToMany(mappedBy = "dailyPostEntity", cascade = CascadeType.ALL)
     private Set<Favorites> favorites;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id",nullable = false)
+    private ClientEntity clientEntity;
 
     @Column(name="created_at")
     private Date createdAt;
